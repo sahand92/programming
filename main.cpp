9f1e24ec71ac29f667e1bbeb1ff498e4ba6199c4
Sahand Assadzadeh <sahanda@ghrian.physics.usyd.edu.au>
2016-09-23 18:07:01 +1000

    1st

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main ()
+{
+ 
+    int i = 39;
+    int j = 20;
+    cout << "sum(i, j): " << i+j << endl; 
+
+    double f1 = 13.5; 
+    double f2 = 20.7; 
+    cout << "subtract(f1, f2): " << i-j << endl; 
+
+    string s1 = "Hello"; 
+    string s2 = "World"; 
+    cout << "s1: " << s1 << endl; 
+
+   return 0;
+}
